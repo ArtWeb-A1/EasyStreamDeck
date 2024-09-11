9cfffcf00255d47b2cfa79761e1c62d7f53583fb
ArtWeb-A1 <top49030@gmail.com>
2024-09-11 23:47:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
+
+
+Config.set('graphics', 'width', '700')
+Config.set('graphics', 'height', '400')
+
+
+class ETStreamDeckApp(App):
+
+    def build(self):
+
+        FL = FloatLayout(size=(700, 500))
+
+        return FL
+
+
+if __name__ == "__main__":
+
+    ETStreamDeckApp().run()
 
